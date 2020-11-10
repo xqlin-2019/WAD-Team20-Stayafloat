0b99336990f9cb94aa02b61b8172fbdb23fe8410
xtemplate24 <71815928+xtemplate24@users.noreply.github.com>
2020-11-10 17:22:21 +0800

    Updated news and relax
    Changed linking for

--- js/news.js
+++ js/news.js
@@ -1,10 +1,12 @@
 var key = "0c73b075b02f486ab230160d61a0f815";
+var key2 = "c2138d7edb6e481a80ee75989ac1c8a9";
 var cors = 'http://cors-anywhere.herokuapp.com/';
 
 
 var country = 'sg';
 var pageSize = '2';
 var first_article = true;
+
 
 
 var today = new Date();
@@ -24,11 +26,13 @@ function shuffleArray(array) {
 
 function retrieve_preference() {
 
-    var preferences = sessionStorage.getItem('preference');
-    if (preferences == "") {
-        return
-    }
-    var topics = preferences.split(',');
+    // var preferences = sessionStorage.getItem('preference');
+    // if (preferences == "") {
+    //     return
+    // }
+    // var topics = preferences.split(',');
+
+    var topics = ["Entertainment","Singapore","Technology","Business"]
     shuffleArray(topics);
 
     for (each of topics){
@@ -39,7 +43,6 @@ function retrieve_preference() {
 function display_default(query){
 
     var request = new XMLHttpRequest();
-    console.log(query);
 
     
 
@@ -54,9 +57,9 @@ function display_default(query){
 
     
     if (query == "Singapore"){
-        var url_query = cors +`http://newsapi.org/v2/top-headlines?country=${country}&apiKey=${key}&pageSize=${pageSize}`;
+        var url_query = cors +`http://newsapi.org/v2/top-headlines?country=${country}&apiKey=${key2}&pageSize=${pageSize}`;
     } else {
-        var url_query = cors +`http://newsapi.org/v2/top-headlines?country=${country}&category=${query}&pageSize=${pageSize}&apiKey=${key}`;
+        var url_query = cors +`http://newsapi.org/v2/top-headlines?country=${country}&category=${query}&pageSize=${pageSize}&apiKey=${key2}`;
     }
     request.open("GET", url_query, true);
     
@@ -66,16 +69,16 @@ function display_default(query){
 
 function display(xml,query){
     var response_json = JSON.parse(xml.responseText);
-    console.log(response_json);
     var news = response_json.articles;
-
-    shuffleArray(news);
+    
     console.log(news);
 
     var str = ``;
     var first = ``;
 
     for (article of news){
+
+
         var description = JSON.stringify(article['description']);
 
         if (description.includes(" ")){
@@ -87,34 +90,42 @@ function display(xml,query){
             if (first_article == true){
                 first += `
                 <div class="jumbotron jumbotron-fluid mx-auto" style="background-image: url(${image}); background-size: 100%; "padding-bottom: 0px">
-                <div class="container" id = "first_article_text" style = "padding-bottom: 0px">
-                <br><br><br><br><br><br>
+                <br><br><br><br><br><br><br><br><br><br><br><br>
+                <div class="container bg-light" id = "first_article_text" style = "padding-bottom: 0px; margin:0px; opacity:0.5">
                 <span class = "badge badge-primary">${query}</span><br></img><h3>${title}</h3>
                 <p>${description}</p>
-                <a href = ${more_info} class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Click to know more</a>
+                <small class="text-muted"><a href = ${more_info}>Click to know more</a></small>
                  </div>
-                            </div>
+                </div>
                 `;
 
                 first_article = false;
             } else {
                 str += `
-                
-                <div class="card" style = "margin: 10px">
-                    <img src="${image}" class="card-img-top" alt="#">
-                    <div class="card-body">
+                <div class="card mb-3 mx-auto border-0" style="max-width: 60%;">
+                    <div class="row no-gutters">
+                    <div class="col-md-4">
+                    <br>
+                        <img src="${image}" class="card-img " alt="#" id = "article_image">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                        <span class = "badge badge-primary">${query}</span>
                         <h5 class="card-title">${title}</h5>
                         <p class="card-text">${description}</p>
                         <p class="card-text"><small class="text-muted"><a href = ${more_info}>Click to know more</a></small></p>
+                        </div>
                     </div>
-                </div>`;
+                    </div>
+                </div>`
+
             }
 
         }
         
     }
     
-    document.getElementById('other_articles').innerHTML += str;
+    document.getElementById('articles').innerHTML += str;
     document.getElementById('first').innerHTML += first;
 
 }
