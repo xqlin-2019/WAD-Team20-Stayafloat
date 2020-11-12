06c618c9213b8458cced3b34aa2a5d479b400ca3
verakeh <kehvera@gmail.com>
2020-11-12 20:25:00 +0800

    update track

--- js/track.js
+++ js/track.js
@@ -31,12 +31,12 @@ function add_milestone(){
     request.onreadystatechange = function() {
         if( this.readyState == 4 && this.status == 200 ) {
             var obj = JSON.parse(this.responseText); // JS JSON object
-            console.log(obj)
+            //console.log(obj)
             if (obj.update_status == "successful"){
-                document.getElementById("update_status").innerHTML = "<span style='background-color: green; color: white;'>New Milestone Added!</span>";
+                document.getElementById("update_status").innerHTML = "<span style='background-color: green;color: white; margin-bottom:10px;'>New Milestone Added!</span>";
             }
             else {
-                document.getElementById("update_status").innerHTML = "<span style='color: red;'>Error occurs</span>";         
+                document.getElementById("update_status").innerHTML = "<span style='color: red;'>An error occured</span>";         
             }
         }
     }
@@ -44,12 +44,34 @@ function add_milestone(){
     var description = document.getElementById("milestone_desc").value;
     var date = document.getElementById("due_date").value;
 
-    console.log(description, date);
-    var url = `./php/userAuth.php?action=addMilestone&email=${email}&description=${description}&date=${date}`;
+    if(description==""||date ==""){
+        document.getElementById("update_status").innerHTML = "<span style='color: red;'>Please fill in both inputs!</span>";         
+        console.log("error");
+        return;
+    }else{
+        var url = `./php/userAuth.php?action=addMilestone&email=${email}&description=${description}&date=${date}`;
+
+    }
+
     request.open("GET", url, true); // synchronous
     request.send();
 }
 
+function countdown(date){
+    //console.log(date);
+    var countdownDate = new Date(date).getTime();
+    var today = new Date().getTime();
+    //console.log(countdownDate);
+
+
+    var distance = countdownDate - today;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    //console.log(days);
+    
+    return days;
+
+}
 
 function get_milestone(){
     login_check();
@@ -61,21 +83,22 @@ function get_milestone(){
         if( this.readyState == 4 && this.status == 200 ) {
             var obj = JSON.parse(this.responseText); // JS JSON object
             console.log(obj)
-            $milestones_arr = obj.milestones
+            milestones_arr = obj.milestones
             if (obj.retrieve_status == "successful"){
                 console.log("hello");
-                for($milestone of $milestones_arr){
-                    console.log($milestone);
-                    // console.log($milestone.date);
-                    var date = $milestone.date;
-                    var count_down = "";
-                    var description = $milestone.description;
+                for(milestone of milestones_arr){
+                    console.log(milestone);
+                    // console.log(milestone.date);
+                    var date = milestone.date;
+                    var count_down = countdown(date);
+                    var description = milestone.description;
+                    //console.log(count_down);
                     
 
-                    str+= `<div class="card text-white" style="max-width: 18rem; background-color: #102B72;">
-                        <div class="card-body">
-                        <h1 class="card-title text-uppercase">Primary card title</h1>
-                        <p class="card-text">${description}</p>
+                    str+= `<div class="card" style="max-width: 18rem; min-height:10rem;">
+                        <div class="card-body text-white" style="padding:0">
+                        <h1 class="card-title text-uppercase" style=" background-color: #102B72; padding:10px; ">${count_down} DAYS LEFT</h1>
+                        <p class="card-text" style="color:black">${description}</p>
                         </div>
                     </div>`;
                 }
@@ -89,11 +112,30 @@ function get_milestone(){
     var url = `./php/userAuth.php?action=getMilestones&email=${email}`;
     request.open("GET", url, true); // synchronous
     request.send();
+}
 
+function display_mood(){
+    var chart = new CanvasJS.Chart("chartContainer",
+    {	
+        axisX:{
+        valueFormatString: "DD MMM",
+        },
+        axisY:{
+        interval: 1
+        },
+        data: [
+        {        
+            type: "splineArea",
+            dataPoints: [
+            {x: new Date(2018, 11, 24), y: 1},
+            {x: new Date(2018, 11, 25), y: 2},     
+            {x: new Date(2018, 11, 27), y: 2},     
+            ]
+        }             
+        ]
+    });
+
+    chart.render();
 
 }
 
-function count_down(){
-    var today = new Date();
-
-}
